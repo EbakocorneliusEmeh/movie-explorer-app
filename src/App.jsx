bbdd165623ae5bd440d51002ed691b099560e7c7
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2025-03-14 12:21:23 +0200

    still working

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+import DetailPage from "./pages/Detailpage";
+import HomePage from "./pages/Homepage";
+import { BrowserRouter, Routes, Route } from "react-router";
+import { MoveData } from "./Context/move";
 
 function App() {
-
   return (
-    <>
-
-    </>
-  )
+    <MoveData>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+        </Routes>
+      </BrowserRouter>
+    </MoveData>
+  );
 }
 
-export default App
+export default App;
