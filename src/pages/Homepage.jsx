d1ac8caf3cd4fb6ff0921fe607cd71260dd56706
EBAKO CORNELIUS <ebakocornelius@gmail.com>
2025-03-21 11:49:19 +0200

    adding to work

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Search from "../components/Seacrch";
